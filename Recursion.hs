6fdf5cff86d2d3936735c687e67a71dc408dab8e
decque@gmail.com <seg@seg-PC.(none)>
2012-06-22 21:03:42 +0900

    refined recuresion.hs

--- Recursion.hs
+++ Recursion.hs
@@ -14,12 +14,6 @@ main = do
 takeStat :: IO Int -> Int -> IO Int
 takeStat io count =
     return $ foldr'' ((+).unsafePerformIO) 0 $ replicate count io
---    return $ foldr' ((+).unsafePerformIO) 0 $ replicate count io
---    return $ foldr' (+) 0 $ replicate count $ unsafePerformIO io
---    foldr'' (liftM2 (+)) (return 0) $ replicate count io
-
-instance NFData a => NFData (IO a) where
-    rnf x = rnf $ unsafePerformIO x
 
 foldr''  :: NFData b => (a->b->b) -> b-> [a] -> b
 foldr'' folder init []          = init
