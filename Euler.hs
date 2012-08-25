12a8367de4d647d80f4ce6224baaafa51e6d1868
dec9ue <decque@gmail.com>
2012-08-25 10:54:57 +0900

    refactored a little more haskell oriented

--- Euler.hs
+++ Euler.hs
@@ -1,26 +1,28 @@
--- main = do
-is_circle n      = (show n) == (reverse $ show n)
-declist :: Integer -> Integer -> Integer ->  [(Integer,Integer)] ->  [(Integer,Integer)]
-declist a b limit tail =
-    if b <= a
-    then (a,b):(declist (a-1) (b+1) limit tail )
-    else tail
-declist_rec a b limit  = declist a b limit $
-    if(a + b > 2)
-    then
-      if (a+b-1) > limit 
-      then declist_rec limit (a + b -1 -limit) limit
-      else declist_rec (a+b-2) 1 limit
-    else [] 
-mult_list       :: [Integer]
-mult_list        = [(x*y) | (x,y) <- declist_rec 999 999 999] 
-first_sol        = head $ filter is_circle mult_list
-search_limit     = (\x->x * (1 + x)) $ (\x -> (999 - x +1)) $ floor $ sqrt $ fromInteger first_sol
 
-main = do
-     print $ "first sol:" ++ show first_sol
-     print $ show $ take 10 mult_list
-     print $ "search limit:" ++ show search_limit
-     print $ maximum $ filter is_circle (take search_limit mult_list)
- 
+is_circle n = is_circle_sub n n 0
+    where
+        is_circle_sub m n r
+            | m == 0 = r == n
+            | otherwise = is_circle_sub (m `div` 10) n (r * 10 + m `mod` 10)
+
+declist a b limit tail
+    | b <= a    = (a,b):(declist (a-1) (b+1) limit tail )
+    | otherwise = tail
+
+-- decendant list of pair (a,b) on value a + b
+declist_rec a b limit
+    | a + b <= 2      = declist a b limit []
+    | (a+b-1) > limit = declist a b limit $ declist_rec limit (a + b -1 -limit) limit
+    | otherwise       = declist a b limit $ declist_rec (a+b-2) 1 limit
+
+mult_list        = map (\ (x,y) -> x * y) $ declist_rec 9999 9999 9999 
+index_mult_list  = zip [1..] mult_list
+
+main =
+    let (index,first_sol) = head $ filter (is_circle.snd) index_mult_list
+        root              = floor $ sqrt $ fromInteger first_sol
+        search_limit      = (\x->x * x) $ (\x -> (9999 - x +1)) $ root
+    in
+    do
+        print $ maximum $ filter is_circle (drop (index-1) $ take search_limit mult_list)
 
