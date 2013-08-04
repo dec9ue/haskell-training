892c6bf8eeb18586fb5326111604a997d38667d5
dec9ue <decque@gmail.com>
2013-08-05 08:36:47 +0900

    rewrote naively Ev4.hs

--- Ev4.hs
+++ Ev4.hs
@@ -1,15 +1,26 @@
 import Control.Parallel.Strategies
 
-evalParListWithN n l = do
-    x <- parListN n rpar l
+parListWithN :: Int -> [a] -> Eval ()
+parListWithN n l = do
+    parSeqListPar $ take n l
     case drop n l of
-      [] -> return []
-      _  -> evalParListWithN n $ drop n l
+      [] -> return ()
+      _  -> parListWithN n $ drop n l
 
+parSeqListPar :: [a] -> Eval ()
+parSeqListPar [] = return ()
+parSeqListPar (hd:tl) = do
+    rpar hd
+    parSeqListPar tl
+    rseq hd
+    return ()
+
+filterEvalN :: (a -> Bool) -> [a] -> [a]
 filterEvalN f l = runEval $ do
-    x <- rpar $ evalParListWithN 10 l
+    rpar $ runEval $ parListWithN 100 l
     return $ filter f l
 
+isPrime :: Integral b => b -> Bool
 isPrime n = not $ or $ map ((==0).(n `mod`)) [2..(n-2)]
 
 -- main = print $ take 100 $ filterEvalN isPrime [43532466234562353523..]
